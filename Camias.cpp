12a447a1d440aff380ed521be05a9f0dcf5d70be
JMMendozaF <jair.mendoza.floresxd@gmail.com>
2024-06-15 09:40:05 -0500

    Se expecificó la variable final, diciendo que es el precio a pagar pero ya descontado

--- Camias.cpp
+++ Camias.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
 	double cant, pcam, monto, final;
 	// pcam es el precio por cada camisa
+	// " final " es el precio total a pagar descontando el descuento
 	cout<<"TOTAL A PAGAR POR CAMISAS"<<endl;
 	cout<<"Introduzca la cantidad de camisas: ";
 	cin>>cant;
